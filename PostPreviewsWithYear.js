02ebc5b59f73818f634431315499779777164e67
Alilotfi09 <ali.lotfi09z9@gmail.com>
2025-08-14 13:49:28 +0330

    Updates

---
+++ PostPreviewsWithYear.js
@@ -0,0 +1,21 @@
+import { b as createAstro, c as createComponent, m as maybeRenderHead, e as addAttribute, r as renderComponent, F as Fragment, a as renderTemplate } from './astro/server.js';
+import 'kleur/colors';
+import { b as $$PostPreview } from './PostPreview.js';
+
+const $$Astro = createAstro("https://blog.imalixd.ir");
+const $$PostPreviewsWithYear = createComponent(($$result, $$props, $$slots) => {
+  const Astro2 = $$result.createAstro($$Astro, $$props, $$slots);
+  Astro2.self = $$PostPreviewsWithYear;
+  const { posts, yearMarkers, class: className } = Astro2.props;
+  let currentYear = 0;
+  return renderTemplate`${maybeRenderHead()}<div${addAttribute(className, "class")}> ${posts.map((post) => {
+    const yearPublished = post.data.published.getFullYear();
+    if (yearMarkers && yearPublished !== currentYear) {
+      currentYear = yearPublished;
+      return renderTemplate`${renderComponent($$result, "Fragment", Fragment, {}, { "default": ($$result2) => renderTemplate` <div class="text-2xl pb-3 my-7 text-foreground/50 border-b-4 border-dotted border-foreground/20"> ${yearPublished} </div> ${renderComponent($$result2, "PostPreview", $$PostPreview, { "post": post })} ` })}`;
+    }
+    return renderTemplate`${renderComponent($$result, "PostPreview", $$PostPreview, { "post": post })}`;
+  })} </div>`;
+}, "C:/Users/AliLotfi/Desktop/myblog/src/components/PostPreviewsWithYear.astro", void 0);
+
+export { $$PostPreviewsWithYear as $ };
---
+++ content-assets.js
@@ -0,0 +1,42 @@
+const __ASTRO_IMAGE_IMPORT_Z2jk40O = new Proxy({"src":"/avatar.jpg","width":1280,"height":1280,"format":"jpg"}, {
+						get(target, name, receiver) {
+							if (name === 'clone') {
+								return structuredClone(target);
+							}
+							if (name === 'fsPath') {
+								return "C:/Users/AliLotfi/Desktop/myblog/src/content/avatar.jpg";
+							}
+							if (target[name] !== undefined && globalThis.astroAsset) globalThis.astroAsset?.referencedImages.add("C:/Users/AliLotfi/Desktop/myblog/src/content/avatar.jpg");
+							return target[name];
+						}
+					});
+
+const __ASTRO_IMAGE_IMPORT_Zs4Ge3 = new Proxy({"src":"/PixelatedGreenTreeSide.png","width":287,"height":304,"format":"png"}, {
+						get(target, name, receiver) {
+							if (name === 'clone') {
+								return structuredClone(target);
+							}
+							if (name === 'fsPath') {
+								return "C:/Users/AliLotfi/Desktop/myblog/src/content/posts/showing-off-blog-features/PixelatedGreenTreeSide.png";
+							}
+							if (target[name] !== undefined && globalThis.astroAsset) globalThis.astroAsset?.referencedImages.add("C:/Users/AliLotfi/Desktop/myblog/src/content/posts/showing-off-blog-features/PixelatedGreenTreeSide.png");
+							return target[name];
+						}
+					});
+
+const __ASTRO_IMAGE_IMPORT_1W6MYL = new Proxy({"src":"/cover.jpg","width":640,"height":676,"format":"jpg"}, {
+						get(target, name, receiver) {
+							if (name === 'clone') {
+								return structuredClone(target);
+							}
+							if (name === 'fsPath') {
+								return "C:/Users/AliLotfi/Desktop/myblog/src/content/posts/showing-off-blog-features/cover.jpg";
+							}
+							if (target[name] !== undefined && globalThis.astroAsset) globalThis.astroAsset?.referencedImages.add("C:/Users/AliLotfi/Desktop/myblog/src/content/posts/showing-off-blog-features/cover.jpg");
+							return target[name];
+						}
+					});
+
+const contentAssets = new Map([["./avatar.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Faddendum.md", __ASTRO_IMAGE_IMPORT_Z2jk40O], ["avatar.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fhome.md", __ASTRO_IMAGE_IMPORT_Z2jk40O], ["./PixelatedGreenTreeSide.png?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fshowing-off-blog-features%2Findex.md", __ASTRO_IMAGE_IMPORT_Zs4Ge3], ["./cover.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fshowing-off-blog-features%2Findex.md", __ASTRO_IMAGE_IMPORT_1W6MYL]]);
+
+export { contentAssets as default };
