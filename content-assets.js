3cafac025dc54c10e954bb2985846c3deafd46f2
Alilotfi09 <ali.lotfi09z9@gmail.com>
2025-08-18 19:29:30 +0330

    Updates

--- content-assets.js
+++ content-assets.js
@@ -1,3 +1,16 @@
+const __ASTRO_IMAGE_IMPORT_Z1Glz99 = new Proxy({"src":"/avatar.jpg","width":1280,"height":1280,"format":"jpg"}, {
+						get(target, name, receiver) {
+							if (name === 'clone') {
+								return structuredClone(target);
+							}
+							if (name === 'fsPath') {
+								return "C:/Users/AliLotfi/Desktop/myblog/src/content/avatar.jpg";
+							}
+							if (target[name] !== undefined && globalThis.astroAsset) globalThis.astroAsset?.referencedImages.add("C:/Users/AliLotfi/Desktop/myblog/src/content/avatar.jpg");
+							return target[name];
+						}
+					});
+
 const __ASTRO_IMAGE_IMPORT_Z190lSx = new Proxy({"src":"/PixelatedGreenTreeSide.png","width":287,"height":304,"format":"png"}, {
 						get(target, name, receiver) {
 							if (name === 'clone') {
@@ -24,19 +37,6 @@ const __ASTRO_IMAGE_IMPORT_1W6MYL = new Proxy({"src":"/cover.jpg","width":640,"h
 						}
 					});
 
-const __ASTRO_IMAGE_IMPORT_Z1Glz99 = new Proxy({"src":"/avatar.jpg","width":1280,"height":1280,"format":"jpg"}, {
-						get(target, name, receiver) {
-							if (name === 'clone') {
-								return structuredClone(target);
-							}
-							if (name === 'fsPath') {
-								return "C:/Users/AliLotfi/Desktop/myblog/src/content/avatar.jpg";
-							}
-							if (target[name] !== undefined && globalThis.astroAsset) globalThis.astroAsset?.referencedImages.add("C:/Users/AliLotfi/Desktop/myblog/src/content/avatar.jpg");
-							return target[name];
-						}
-					});
-
-const contentAssets = new Map([["PixelatedGreenTreeSide.png?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fshowing-off-blog-features%2Findex.md", __ASTRO_IMAGE_IMPORT_Z190lSx], ["./cover.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fshowing-off-blog-features%2Findex.md", __ASTRO_IMAGE_IMPORT_1W6MYL], ["./avatar.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Faddendum.md", __ASTRO_IMAGE_IMPORT_Z1Glz99]]);
+const contentAssets = new Map([["./avatar.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Faddendum.md", __ASTRO_IMAGE_IMPORT_Z1Glz99], ["PixelatedGreenTreeSide.png?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fshowing-off-blog-features%2Findex.md", __ASTRO_IMAGE_IMPORT_Z190lSx], ["./cover.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fshowing-off-blog-features%2Findex.md", __ASTRO_IMAGE_IMPORT_1W6MYL]]);
 
 export { contentAssets as default };
